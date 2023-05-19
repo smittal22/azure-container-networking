b4a944f834f406466d2f2eb06a7f036ca4ac7b5d
Saksham Mittal <sakmittal@microsoft.com>
2023-05-19 11:37:24 -0700

    initial getHomeAZ 404 changes

--- cns/restserver/homeazmonitor.go
+++ cns/restserver/homeazmonitor.go
@@ -126,6 +126,12 @@ func (h *HomeAzMonitor) Populate(ctx context.Context) {
 				h.update(returnCode, returnMessage, cns.HomeAzResponse{IsSupported: true})
 				return
 
+			case http.StatusNotFound:
+				returnMessage := fmt.Sprintf("[HomeAzMonitor] region does not support AZs probably, failed with StatusCode: %d, error: %v", apiError.StatusCode(), err)
+				returnCode := types.NotFound
+				h.update(returnCode, returnMessage, cns.HomeAzResponse{IsSupported: false})
+				return
+
 			default:
 				returnMessage := fmt.Sprintf("[HomeAzMonitor] failed with StatusCode: %d", apiError.StatusCode())
 				returnCode := types.UnexpectedError
